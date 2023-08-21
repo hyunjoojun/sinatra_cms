6d97886f2ada55891d6e65d3828729f61b2eb652
Hyunjoo Jun <hjj102088@gmail.com>
2023-08-21 16:04:04 -0400

    index

--- cms.rb
+++ cms.rb
@@ -3,6 +3,11 @@ require 'sinatra/reloader'
 require 'sinatra/content_for'
 require 'tilt/erubis'
 
+root = File.expand_path('..', __FILE__)
+
 get '/' do
+  @files = Dir.glob("#{root}/data/*").map do |path|
+    File.basename(path)
+  end
   erb :index
 end
