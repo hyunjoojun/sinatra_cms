126d9b7096ecd93e9a0a70ce25e9a1617d9a7f91
Hyunjoo Jun <hjj102088@gmail.com>
2023-08-22 12:50:31 -0400

    not found

--- cms.rb
+++ cms.rb
@@ -6,6 +6,11 @@ require 'pry'
 
 root = File.expand_path('..', __FILE__)
 
+configure do
+  enable :sessions
+  set :session_secret, SecureRandom.hex(32)
+end
+
 get '/' do
   @files = Dir.glob("#{root}/data/*").map do |path|
     File.basename(path)
@@ -16,6 +21,11 @@ end
 get '/:filename' do
   file_path = "#{root}/data/#{params[:filename]}"
 
-  headers['Content-Type'] = 'text/plain'
-  File.read(file_path)
+  if File.file?(file_path)
+    headers['Content-Type'] = 'text/plain'
+    File.read(file_path)
+  else
+    session[:message] = "#{params[:filename]} does not exist."
+    redirect '/'
+  end
 end
--- test/cms_test.rb
+++ test/cms_test.rb
@@ -29,4 +29,17 @@ class CMSTest < Minitest::Test
     assert_equal 'text/plain', last_response['Content-Type']
     assert_includes last_response.body, 'Ruby is a language of careful balance.'
   end
+
+  def test_document_not_found
+    get '/nofile.txt'
+
+    assert_equal 302, last_response.status
+    get last_response['Location']
+
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, 'nofile.txt does not exist'
+
+    get '/'
+    refute_includes last_response.body, 'nofile.txt does not exist'
+  end
 end
